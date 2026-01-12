3b8e0b471da39f6b051f9848b2cfa62c337fbd15
IJU666 <fauzimaulanabandung2020@gmail.com>
2026-01-12 19:14:02 +0700

    tudey

---
+++ checkout.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg bg" style="background-color: #253898;">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#"><b>NGAJUAL</b></a>
+    
+          <a class="nav-link text-light" href="#">Pusat Penjual</a>
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#"><i class="fas fa-fw fa-bell"></i>Notifikasi</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light disabled"><i class="fas fa-fw fa-users"></i> Masuk</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<img src="img/circleatas.png" width="7%" alt="" class="position-absolute ">
+<img src="img/circlebawah.png" width="7%" alt="" class="position-absolute bottom-0 end-0">
+
+    <div class="container" style="margin-top: 10%;">
+            <div class="card px-4 py-2 shadow-lg">
+                <p class="fs-5">Detail Transaksi</p>
+                <div class="d-flex justify-content-between">
+                    <p class="fw-bold"><i class="fas fa-fw fa-map-marker-alt" style="color: #253898;"></i>Drop Point</p>
+                    <a href="" class="btn btn-primary">Ubah</a>
+            </div>
+            <p>Lantai 5 Universitas Muhammadiyah Bandung</p>
+        </div>
+        <!-- card2 -->
+            <div class="card px-4 py-2 mt-3 shadow-lg">
+                <small class="fw-semibold mb-3">Produk Dipesan</small>
+                <div class="d-flex justify-content-between">
+                    <p class="fw-bold"><i class="fas fa-fw fa-map-marker-alt" style="color: #253898;"></i>Juned</p>
+                    <a href="" class="btn btn-primary">Ubah</a>
+            </div>
+                <div class="d-flex">
+                    <img src="img/cilok.png" alt="" srcset="">
+                    <p class="d-flex justify-content-center align-items-center fw-semibold ms-3">Cilok Bumbu Kacang</p>
+                </div>
+                <p class="fw-semibold mb-3">Rincian Pembayaran</p>
+                <table class="col-12">
+                    <tr>
+                        <td>Total Harga (1 Barang)</td>
+                        <td class="text-end">Rp 10.000</td>
+                    </tr>
+                    <tr>
+                        <td>Biaya Admin</td>
+                        <td class="text-end">Rp 2.000</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
