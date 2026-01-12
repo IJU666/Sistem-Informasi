a26b3bfbde05d752ceb8588e8015ab998b8f66b1
ahnaf2410 <musyaffaahnaf1@gmail.com>
2026-01-12 19:58:54 +0700

    Detail produk, login

--- admin_page/login.php
+++ admin_page/login.php
@@ -18,63 +18,73 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
 
 </head>
 
-<body class="bg-gradient-primary">
+<body class="">
 
     <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="row justify-content-center align-items-center min-vh-100 ">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="col-xl-10 col-lg-12 col-md-9 min-vh-75 " >
+                <div class="card o-hidden border-0 rounded-lg shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6 d-flex justify-content-center align-items-center flex-column position-relative  text-white">
+                                <img src="./img/login.png" alt="" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover">
+                                <h1 class="position-relative" style="z-index: 1;">
+                                    <span>Hallo</span>
+                                    <span class="d-block">Selamat Datang!</span>
+                                </h1>
+                                <p class="position-relative" style="z-index: 1;">Masuk untuk memulai belanja. <br>
+                                    Nikmati penawaran lainnya di <b>ngejual!</b> </p>
+                            </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
                                     </div>
                                     <form class="user">
-                                        <div class="form-group">
+                                        <div class="form-group mb-3">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Masukkan alamat email...">
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group mb-3">
                                             <input type="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Password">
                                         </div>
                                         <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                            <div
+                                                class="custom-control custom-checkbox small mb-3 justify-content-between d-flex">
+                                                <div class="gap-2">
+                                                    <input type="checkbox" class="custom-control-input"
+                                                        id="customCheck">
+                                                    <label class="custom-control-label" for="customCheck">Ingat saya</label>
+                                                </div>
+                                                <a class="small" href="forgot-password.php">Lupa Password?</a>
+                                            </div>
+                                            <div class="justify-content-between d-flex">
+                                                <button class="btn text-light col-5" type="button" style="background-color: #243796;">Login</button>
+                                                <button class="btn border shadow col-5" type="button">Sign Up</button>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        <a href="index.html" class="btn  btn-user btn-block col-12 mt-3 border shadow">
+                                            <i class="fab fa-google fa-fw"></i> Masuk Dengan Google
                                         </a>
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href="register.php">Buat Akun Baru</a>
                                     </div>
                                 </div>
                             </div>
@@ -96,7 +106,9 @@
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
 
 </body>
 
