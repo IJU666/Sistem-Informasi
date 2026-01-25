35a2cc024e9f3f44f82e7db973e61fcfcaf141cf
IJU666 <fauzimaulanabandung2020@gmail.com>
2026-01-25 23:59:02 +0700

    done

--- auth/register.php
+++ auth/register.php
@@ -108,7 +108,7 @@ include '../includes/header.php';
                              
                             </div>
                             <div class="text-center">
-                                <a class="small" href="login.html">Already have an account? Login!</a>
+                                <a class="small" href="login.php">Already have an account? Login!</a>
                             </div>
                         </div></div>
                     <div class="col-lg-6 position-relative overflow-hidden">
